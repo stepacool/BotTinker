076dd05161a8a6e2dd069a623f547629593b4443
Stephen <stepanfilonov@gmail.com>
2018-09-08 19:53:34 +0700

    Connection, users, threading

--- bot.py
+++ bot.py
@@ -1,1 +1,33 @@
-bot.py
+import config
+import utils
+import requests
+import json
+import re
+import threading
+import socket
+from time import sleep
+
+
+
+
+def main():
+	s = socket.socket()
+	s.connect((config.HOST, config.PORT))
+	s.send("NAME {}\r\n".format(config.NAME).encode("utf-8"))
+	s.send("PASS {}\r\n".format(config.PASS).encode("utf-8"))
+	s.send("JOIN #{}\r\n".format(config.CHAN).encode("utf-8"))
+
+	chat_message = re.compile 
+	utils.mess(s, "Hello. Bot initiated.")
+
+	T = threading.Thread(args = (), target = utils.fillModList, group=None)
+	T.start()
+	T.run()
+	while True:
+		response = s.recv(1024).decode('utf-8')
+		print(response)
+		sleep(2)
+
+
+if __name__ == "__main__":
+	main()
--- utils.py
+++ utils.py
@@ -1,14 +1,13 @@
 import config
 import requests
 import json
-import time
 import threading
 import socket
 from time import sleep
 
 
 def mess(sock, mess):
-	sock.send("PRIVMSG #{} :{}\r\n".format(cofig.CHAN, message))
+	sock.send("PRIVMSG #{} :{}\r\n".format(config.CHAN, mess).encode('utf-8'))
 
 
 
@@ -28,7 +27,7 @@ def timeout(sock, user, seconds = 500):
 
 
 #http://tmi.twitch.tv/group/user/stepacool/chatters
-def fillOpList():
+def fillModList():
 	while True:
 		try:
 			url = "http://tmi.twitch.tv/group/user/stepacool/chatters"
@@ -42,21 +41,10 @@ def fillOpList():
 					config.modlist[p] = "mod"
 				for p in data["chatters"]["admins"]:
 					config.modlist[p] = "admin"
+				print(data)
 		except:
 			print("ERROR! Something went wrong.")
 		sleep(5)
 
 
 
-
-url = "http://tmi.twitch.tv/group/user/stepacool/chatters"
-req = requests.get(url, headers={"accept": "*/*"})
-res = req.text
-print(req, res, req.json())
-if not res.find("502 Bad Gateway"):
-	config.modlist.clear()
-	data = req.json()
-	for p in data["chatters"]["moderators"]:
-		config.modlist[p] = "mod"
-	for p in data["chatters"]["admins"]:
-		config.modlist[p] = "admin"
